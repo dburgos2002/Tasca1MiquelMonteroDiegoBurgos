f6c5c497b00833c41cd9da386053a5ed3d6da87a
MiquelAngelMontero <mmontero2@institutjaumehuguet.cat>
2022-10-06 16:19:48 +0200

    commitMiquel06102022

--- app/src/main/java/com/example/tasca1miquelmonterodiegoburgos/MainActivity.java
+++ app/src/main/java/com/example/tasca1miquelmonterodiegoburgos/MainActivity.java
@@ -18,16 +18,16 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    final double[] secImpr = {0};
+    final double[] minImpr = {0};
+    final double[] horImpr = {0};
+    final double[] anyImpr = {0};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        final int[] valor = {0};
+        final double[] valor = {0};
         final String[] unitat = {""};
-
-        int secImpr = 0;
-        int minImpr = 0;
-        int horImpr = 0;
-        int anyImpr = 0;
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -36,13 +36,13 @@ public class MainActivity extends AppCompatActivity {
         final Spinner sp1 = (Spinner)findViewById(R.id.unitats);
 
         final TextView sec = (TextView)findViewById(R.id.seg);
-        sec.setText(String.valueOf(secImpr));
+        sec.setText(String.valueOf(secImpr[0]));
         final TextView min = (TextView)findViewById(R.id.min);
-        min.setText(String.valueOf(minImpr));
+        min.setText(String.valueOf(minImpr[0]));
         final TextView hor = (TextView)findViewById(R.id.hor);
-        hor.setText(String.valueOf(horImpr));
+        hor.setText(String.valueOf(horImpr[0]));
         final TextView any = (TextView)findViewById(R.id.an);
-        any.setText(String.valueOf(anyImpr));
+        any.setText(String.valueOf(anyImpr[0]));
 
         Button button= (Button) findViewById(R.id.convertir);
         button.setOnClickListener(new View.OnClickListener() {
@@ -50,7 +50,89 @@ public class MainActivity extends AppCompatActivity {
                 valor[0] = Integer.parseInt(et1.getText().toString());
                 unitat[0] = sp1.getSelectedItem().toString();
                 System.out.println("Valor es: " + valor[0] + "\n La unitat és: " + unitat[0]);
+
+                secImpr[0] = calcularSec(unitat[0], valor[0]);
+                sec.setText(String.valueOf(secImpr[0]));
+
+                minImpr[0] = calcularMin(unitat[0], valor[0]);
+                min.setText(String.valueOf(minImpr[0]));
+
+                horImpr[0] = calcularHor(unitat[0], valor[0]);
+                hor.setText(String.valueOf(horImpr[0]));
+
+                anyImpr[0] = calcularAny(unitat[0], valor[0]);
+                any.setText(String.valueOf(anyImpr[0]));
             }
         });
     }
+
+    public double calcularSec(String unitat, double valor) {
+
+        switch (unitat) {
+            case "Segons":
+                return valor;
+            case "Minuts":
+                return valor*60;
+            case "Hores":
+                return valor*3600;
+            case "Anys":
+                return valor*31536000;
+            default:
+                return 0;
+        }
+
+    }
+
+    public double calcularMin(String unitat, double valor) {
+
+        switch (unitat) {
+            case "Segons":
+                return valor/60;
+            case "Minuts":
+                return valor;
+            case "Hores":
+                return valor*60;
+            case "Anys":
+                return valor*525600;
+            default:
+                return 0;
+        }
+
+    }
+
+    public double calcularHor(String unitat, double valor) {
+
+        switch (unitat) {
+            case "Segons":
+                System.out.println("El resultado es: " + valor/3600);
+                return valor/3600;
+            case "Minuts":
+                return valor/60;
+            case "Hores":
+                return valor;
+            case "Anys":
+                return valor*8760;
+            default:
+                return 0;
+        }
+
+    }
+
+    public double calcularAny(String unitat, double valor) {
+
+        switch (unitat) {
+            case "Segons":
+                return valor/31536000;
+            case "Minuts":
+                return valor/525600;
+            case "Hores":
+                return valor/8760;
+            case "Anys":
+                return valor;
+            default:
+                return 0;
+        }
+
+    }
+
 }
